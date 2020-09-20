7af8e2c9d46128d67148ca1baead9236a8896f2d
José Carlos <josecarlosskar@gmail.com>
2020-09-20 18:38:52 -0300

    Add main.rs

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// A language of script - version alpha
+
+const GL_VERSION: &str = "0.1.0-alpha"; // Current version
+
+// Function main
+fn main() {
+	println!("Hello, world! I am glanguage v{}.", GL_VERSION);
+}
