97b6e84dff93e4000bdb208a1bd7f96626ed5973
José Carlos <josecarlosskar@gmail.com>
2020-09-24 15:43:25 -0300

    update 'main' function: add support for cli

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 // A language of script - version alpha
 
+mod cli;
+
 const GL_VERSION: &str = "0.1.0-alpha"; // Current version
 
 // Function main
 fn main() {
-	println!("Hello, world! I am glanguage v{}.", GL_VERSION);
+	let matches = cli::parse_args(GL_VERSION);
+
+	// Check which command was called
+	match matches.subcommand() {
+		_ => (),
+	}
 }
