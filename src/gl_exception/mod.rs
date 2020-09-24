1d0c2434d99696a8508f83bc8c3b8c0628f0d7fb
José Carlos <josecarlosskar@gmail.com>
2020-09-24 16:15:10 -0300

    create function 'new' for component: Exception

--- src/gl_exception/mod.rs
+++ src/gl_exception/mod.rs
@@ -7,3 +7,19 @@ pub struct Exception {
 	column_end: usize,
 	lineno: usize,
 }
+
+impl Exception {
+	pub fn new(
+		filename_ref: &String,
+		linetext_ref: &String,
+		column_start: usize,
+		column_end: usize,
+		lineno: usize,
+	) -> Exception {
+		let filename: String = String::from(filename_ref);
+		let mut linetext: String = String::from(linetext_ref);
+		linetext = linetext.trim_end().to_string();
+		linetext = linetext.replace("\t", " ");
+		Exception { filename, linetext, column_start, column_end, lineno }
+	}
+}
