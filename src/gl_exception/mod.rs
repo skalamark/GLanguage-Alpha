644492cc0f449e6296de3a503c9b2e98d1b01d7f
José Carlos <josecarlosskar@gmail.com>
2020-09-24 16:26:20 -0300

    create method 'generate_exception_string' for component: Exception

--- src/gl_exception/mod.rs
+++ src/gl_exception/mod.rs
@@ -22,4 +22,33 @@ impl Exception {
 		linetext = linetext.replace("\t", " ");
 		Exception { filename, linetext, column_start, column_end, lineno }
 	}
+
+	fn generate_exception_string(
+		&mut self,
+		error_details: &str,
+	) -> String {
+		let mut pows: String = String::from("^");
+		let mut spaces: String = String::new();
+		let mut width_linetext: usize = self.linetext.len();
+
+		self.linetext = self.linetext.trim_start().to_string();
+		width_linetext = width_linetext - self.linetext.len();
+		let new_column_start: usize = self.column_start - width_linetext;
+		let new_column_end: usize = self.column_end - width_linetext;
+
+		for _ in new_column_end..new_column_start {
+			pows.push_str("^");
+		}
+
+		for _ in 0..new_column_start {
+			spaces += " ";
+		}
+
+		let mut result: String = String::new();
+		result.push_str(format!("  File \"{}\", line {}\n", &self.filename, self.lineno).as_str());
+		result.push_str(format!("    {}\n", self.linetext).as_str());
+		result.push_str(format!("    {}{}\n", spaces, pows).as_str());
+		result.push_str(error_details);
+		result
+	}
 }
