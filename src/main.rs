c97ab7e34ca9404891a33feb1f8d0d28262ce400
José Carlos <josecarlosskar@gmail.com>
2020-09-25 17:33:16 -0300

    update file: main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 // A language of script - version alpha
 
 mod cli;
+mod gl_env;
+mod gl_exception;
+mod gl_lexer;
+mod gl_token;
+mod gl_token_position;
+mod gl_tokens;
 
 const GL_VERSION: &str = "0.1.0-alpha"; // Current version
 
