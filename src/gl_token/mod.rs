f5f2c4eaa1c908203e8832a68dc9ec63daed4bff
José Carlos <josecarlosskar@gmail.com>
2020-09-27 17:31:05 -0300

    create method 'illegal_char' for component: Token

--- src/gl_token/mod.rs
+++ src/gl_token/mod.rs
@@ -1,5 +1,7 @@
 // Token
 
+use crate::gl_env::Env;
+use crate::gl_exception::Exception;
 use crate::gl_token_position::TokenPosition;
 use crate::gl_tokens::Tokens;
 
@@ -8,4 +10,20 @@ pub struct Token {
 	pub linetext: String,
 	pub position_start: TokenPosition,
 	pub position_end: TokenPosition,
+}
+
+impl Token {
+	pub fn illegal_char(
+		&self,
+		env: &Env,
+	) {
+		let mut exception: Exception = Exception::new(
+			&env.filename.to_string(),
+			&self.linetext,
+			self.position_start.column,
+			self.position_end.column,
+			env.lineno,
+		);
+		exception.illegal_char();
+	}
 }
