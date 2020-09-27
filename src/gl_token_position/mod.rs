ace963f25f636dd71270904c5f75964caa02de8e
José Carlos <josecarlosskar@gmail.com>
2020-09-27 17:24:10 -0300

    create method 'copy' for component: TokenPosition

--- src/gl_token_position/mod.rs
+++ src/gl_token_position/mod.rs
@@ -4,3 +4,9 @@ pub struct TokenPosition {
 	pub column: usize,
 	pub lineno: usize,
 }
+
+impl TokenPosition {
+	pub fn copy(&self) -> TokenPosition {
+		TokenPosition { column: self.column, lineno: self.lineno }
+	}
+}
