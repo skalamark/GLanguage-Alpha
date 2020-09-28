deccb1df5bfcb9db50b8afc1f46278d59cc00d2d
José Carlos <josecarlosskar@gmail.com>
2020-09-28 17:12:02 -0300

    create function 'new' for component: Interpreter

--- src/gl_interpreter/mod.rs
+++ src/gl_interpreter/mod.rs
@@ -1,7 +1,15 @@
 // Interpreter
 
 use crate::gl_env::Env;
+use crate::gl_lexer::Lexer;
 
 pub struct Interpreter {
 	env: Env,
+}
+
+impl Interpreter {
+	pub fn new(filename: &str) -> Interpreter {
+		let env: Env = Env { filename: filename.to_string(), lineno: 0 };
+		Interpreter { env }
+	}
 }
